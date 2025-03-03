e0c166640c093d582d06138972f27278f755f922
solely-flerken <solely.flerken@proton.me>
2025-03-03 02:19:15 +0100

    ✨ util MouseToWorldPosition

---
+++ Assets/Scripts/Utils/MouseUtils.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public static class MouseUtils
+    {
+        public static Vector3? MouseToWorldPosition(Camera camera, LayerMask? layerMask = null)
+        {
+            var ray = camera.ScreenPointToRay(Input.mousePosition);
+            
+            var maskToUse = layerMask ?? Physics.DefaultRaycastLayers;
+            
+            return Physics.Raycast(ray, out var hit, Mathf.Infinity, maskToUse) ? hit.point : null;
+        }
+    }
+}
