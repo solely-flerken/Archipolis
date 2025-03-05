2ec2afc738c308182bc28438ed13a0c40142f2b2
solely-flerken <solely.flerken@proton.me>
2025-03-05 01:08:38 +0100

    ✨ build placement with snapping

---
+++ Assets/Scripts/Buildings/Building.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Buildings
+{
+    public class Building : MonoBehaviour, IClickable
+    {
+        public void OnClick(GameObject obj)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Buildings/BuildingManager.cs
@@ -0,0 +1,49 @@
+﻿using Events;
+using HexGrid;
+using UnityEngine;
+using Utils;
+
+namespace Buildings
+{
+    public class BuildingManager : MonoBehaviour
+    {
+        private GameObject _selectedBuilding;
+
+        private void Start()
+        {
+            EventSystem.Instance.OnClickableClick += HandleBuildingClick;
+        }
+
+        private void Update()
+        {
+            if (!_selectedBuilding) return;
+            
+            var mouseToWorldPosition = MouseUtils.MouseToWorldPosition(Camera.main);
+            
+            if (mouseToWorldPosition is not { } mousePosition) return;
+
+            mousePosition.y = 0;
+            
+            var cell = HexGridManager.HexGrid.GetNearestHexCell(mousePosition);
+            _selectedBuilding.transform.position = cell.transform.position;
+        }
+
+        private void OnDestroy()
+        {
+            EventSystem.Instance.OnClickableClick -= HandleBuildingClick;
+        }
+
+        private void HandleBuildingClick(GameObject obj)
+        {
+            if (_selectedBuilding is not null)
+            {
+                EventSystem.Instance.InvokeBuildingPlaced(_selectedBuilding);
+                _selectedBuilding = null;
+            }
+            else if (obj.TryGetComponent<Building>(out var newSelectedBuilding))
+            {
+                _selectedBuilding = newSelectedBuilding.gameObject;
+            }
+        }
+    }
+}
