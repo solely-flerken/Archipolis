f232130a131c88b6bc9e66f9771f319d3825a081
solely-flerken <solely.flerken@proton.me>
2025-03-05 01:10:20 +0100

    💩 test something

---
+++ Assets/Scripts/Buildings/Placement.cs
@@ -0,0 +1,29 @@
+﻿using HexGrid;
+using UnityEngine;
+using Utils;
+
+namespace Buildings
+{
+    public class Placement : MonoBehaviour
+    {
+        private HexGrid.HexGrid _hexGrid;
+
+        private void Start()
+        {
+            _hexGrid = HexGridManager.HexGrid;
+        }
+
+        private void Update()
+        {
+            var worldPosition = MouseUtils.MouseToWorldPosition(Camera.main);
+            if (!worldPosition.HasValue) return;
+
+            var hexCoordinates = _hexGrid.WorldToHex(worldPosition.Value);
+            var cell = _hexGrid.GetCell(hexCoordinates.q, hexCoordinates.r);
+            if (!cell) return;
+
+            var mesh = cell.GetComponent<HexMesh>();
+            mesh.ChangeColor(new Color(0.2f, 0.8f, 0.4f));
+        }
+    }
+}
