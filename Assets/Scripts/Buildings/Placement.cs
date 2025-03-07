f4daadc7c5301407da032fb6696f618b1678c4b1
solely-flerken <solely.flerken@proton.me>
2025-03-08 00:49:26 +0100

    💄 visualize occupied hexes

--- Assets/Scripts/Buildings/Placement.cs
+++ Assets/Scripts/Buildings/Placement.cs
@@ -16,16 +16,27 @@ namespace Buildings
         {
             EventSystem.Instance.OnBuildingPlaced -= CheckHexGrid;
         }
-        
-        private static void CheckHexGrid(GameObject obj)
+
+        private void Update()
         {
-            var cells = HexGridManager.Instance.HexGrid.hexCells;
+            var cells = HexGridManager.Instance.hexGrid.hexCells;
 
             foreach (var cell in cells)
             {
                 if(!cell.TryGetComponent<HexMesh>(out var mesh)) continue;
                 mesh.ChangeColor(cell.Occupied ? new Color(0.8f, 0.3f, 0.5f) : new Color(0f, 0f, 0f, 0f));
             }
+        }
+
+        private static void CheckHexGrid(GameObject obj)
+        {
+            // var cells = HexGridManager.Instance.hexGrid.hexCells;
+            //
+            // foreach (var cell in cells)
+            // {
+            //     if(!cell.TryGetComponent<HexMesh>(out var mesh)) continue;
+            //     mesh.ChangeColor(cell.Occupied ? new Color(0.8f, 0.3f, 0.5f) : new Color(0f, 0f, 0f, 0f));
+            // }
         }
     }
 }
