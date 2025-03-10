41047361a4b86b0246e578c3a1020264be347b59
solely-flerken <solely.flerken@proton.me>
2025-03-10 23:39:38 +0100

    ✨ util GetPointOnSphere

---
+++ Assets/Scripts/Utils/SphereUtils.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+namespace Utils
+{
+    public static class SphereUtils
+    {
+        public static Vector3 GetPointOnSphere(float radius, float theta, float phi)
+        {
+            var x = radius * math.sin(phi) * math.cos(theta);
+            var y = radius * math.sin(phi) * math.sin(theta);
+            var z = radius * math.cos(phi);
+
+            // Notice that we need to set y as z here, because unity's up vector is y not z
+            return new Vector3(x, z, y);
+        }
+    }
+}
