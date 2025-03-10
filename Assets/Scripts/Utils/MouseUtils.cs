1a3f960c225203485a06d7025b694c6cfdf2b372
solely-flerken <solely.flerken@proton.me>
2025-03-10 23:40:00 +0100

    ✨ extend mouse utils

--- Assets/Scripts/Utils/MouseUtils.cs
+++ Assets/Scripts/Utils/MouseUtils.cs
@@ -6,9 +6,26 @@ namespace Utils
 {
     public static class MouseUtils
     {
-        public static Vector3 MouseToWorldPosition(Camera camera, Vector3 planeNormal, LayerMask? layerMask = null)
+        public static Vector2 ScreenCenter => new(Screen.width / 2f, Screen.height / 2f);
+        public static Vector2 NormalizedMousePosition(this Vector2 vector) => vector - ScreenCenter;
+
+        public static Vector3 MouseToWorldPosition(Vector3 planeNormal, Camera camera, LayerMask? layerMask = null)
+        {
+            return ScreenPointToWorldPosition(InputManager.Instance.MousePosition, planeNormal, camera, layerMask);
+        }
+
+        public static GameObject GetObjectUnderMouse(Camera camera, LayerMask? layerMask = null)
         {
             var ray = camera.ScreenPointToRay(InputManager.Instance.MousePosition);
+            var maskToUse = layerMask ?? Physics.DefaultRaycastLayers;
+
+            return Physics.Raycast(ray, out var hit, Mathf.Infinity, maskToUse) ? hit.collider.gameObject : null;
+        }
+
+        public static Vector3 ScreenPointToWorldPosition(Vector3 screenPoint, Vector3 planeNormal, Camera camera,
+            LayerMask? layerMask = null)
+        {
+            var ray = camera.ScreenPointToRay(screenPoint);
             var maskToUse = layerMask ?? Physics.DefaultRaycastLayers;
 
             // Perform raycast to check if something is hit
@@ -19,17 +36,9 @@ namespace Utils
 
             // If no hit, calculate where the ray intersects with the defined plane
             var rayToPlaneDistance = -Vector3.Dot(ray.origin, planeNormal) / Vector3.Dot(ray.direction, planeNormal);
-            
+
             // Get the world position of the intersection with the plane
             return ray.origin + ray.direction * rayToPlaneDistance;
-        }
-
-        public static GameObject GetObjectUnderMouse(Camera camera, LayerMask? layerMask = null)
-        {
-            var ray = camera.ScreenPointToRay(InputManager.Instance.MousePosition);
-            var maskToUse = layerMask ?? Physics.DefaultRaycastLayers;
-
-            return Physics.Raycast(ray, out var hit, Mathf.Infinity, maskToUse) ? hit.collider.gameObject : null;
         }
     }
 }
