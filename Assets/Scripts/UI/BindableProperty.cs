ba47d1f969c56b51a93b001ea5e36f4e6323f774
solely-flerken <solely.flerken@proton.me>
2025-04-11 23:20:26 +0200

    ✨ UI data binding

---
+++ Assets/Scripts/UI/BindableProperty.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Unity.Properties;
+
+namespace UI
+{
+    public class BindableProperty<T>
+    {
+        private readonly Func<T> _getter;
+        private readonly Action<T> _setter;
+
+        private BindableProperty(Func<T> getter, Action<T> setter)
+        {
+            _getter = getter;
+            _setter = setter;
+        }
+
+        [CreateProperty]
+        public T Value
+        {
+            get => _getter();
+            set
+            {
+                if (_setter == null)
+                    throw new InvalidOperationException("Setter not defined for this BindableProperty.");
+                _setter(value);
+            }
+        }
+
+        public static BindableProperty<T> Bind(Func<T> getter, Action<T> setter = null) => new(getter, setter);
+    }
+}
---
+++ Assets/Scripts/UI/BindingExtension.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Properties;
+using UnityEngine.UIElements;
+
+namespace UI
+{
+    public static class BindingExtension
+    {
+        public static void Bind<TElement, TValue>(this TElement element, BindableProperty<TValue> property,
+            string targetPropertyName, BindingMode bindingMode = BindingMode.ToTarget)
+            where TElement : VisualElement
+        {
+            element.dataSource = property;
+            element.SetBinding(targetPropertyName, new DataBinding
+            {
+                dataSourcePath = new PropertyPath(nameof(BindableProperty<TValue>.Value)),
+                bindingMode = bindingMode
+            });
+        }
+    }
+}
