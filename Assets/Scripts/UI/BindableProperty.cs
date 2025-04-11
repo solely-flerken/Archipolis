1f5082582b11e414042edf6fd9c634d6a1233ef4
solely-flerken <solely.flerken@proton.me>
2025-04-11 23:57:48 +0200

    📝 documentation for BindableProperty

--- Assets/Scripts/UI/BindableProperty.cs
+++ Assets/Scripts/UI/BindableProperty.cs
@@ -3,6 +3,11 @@ using Unity.Properties;
 
 namespace UI
 {
+    /// <summary>
+    /// A bindable property that allows for easy binding between a property and a UI element.
+    /// Provides getter and setter functionality for synchronization between data and UI.
+    /// </summary>
+    /// <typeparam name="T">The type of the property value.</typeparam>
     public class BindableProperty<T>
     {
         private readonly Func<T> _getter;
@@ -14,6 +19,9 @@ namespace UI
             _setter = setter;
         }
 
+        /// <summary>
+        /// The CreateProperty attribute marks the property to be recognized by Unity's internal property system, enabling it to be used in data binding with Unity's UI Toolkit
+        /// </summary>
         [CreateProperty]
         public T Value
         {
@@ -26,6 +34,12 @@ namespace UI
             }
         }
 
+        /// <summary>
+        /// Creates a new instance of <see cref="BindableProperty{T}"/>.
+        /// </summary>
+        /// <param name="getter">The function to get the value of the property.</param>
+        /// <param name="setter">The action to set the value of the property.</param>
+        /// <returns>A new instance of the <see cref="BindableProperty{T}"/> class.</returns>
         public static BindableProperty<T> Bind(Func<T> getter, Action<T> setter = null) => new(getter, setter);
     }
 }
