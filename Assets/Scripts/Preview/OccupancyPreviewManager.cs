529f5ea4c24fb7e16ba35899acc5bbbc9fabd470
solely-flerken <solely.flerken@proton.me>
2025-04-27 03:10:11 +0200

    ✨ (occupancy) preview manager

---
+++ Assets/Scripts/Preview/OccupancyPreviewManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Hex;
+using Terrain;
+using UnityEngine;
+
+namespace Preview
+{
+    // TODO: Maybe redo this. We use GPU instancing and need to draw every frame. Instead we could dynamically calculate the mesh by removing/adding parts (hexes) of the mesh
+    public class OccupancyPreviewManager : MonoBehaviour
+    {
+        public static OccupancyPreviewManager Instance;
+
+        private readonly Dictionary<HexCoordinate, Matrix4x4> _previewHexes = new();
+
+        private Mesh _hexTemplate;
+
+        [SerializeField] private Material occupancyPreviewMaterial;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+
+            _hexTemplate = Util.GenerateHexagonMesh(MapGenerator.HexRadius, Color.white, MapGenerator.IsFlatTopped);
+
+            // Position the preview slightly above the terrain to avoid z-fighting
+            transform.position = new Vector3(0, 0.05f, 0);
+        }
+
+        private void Update()
+        {
+            Graphics.DrawMeshInstanced(_hexTemplate, 0, occupancyPreviewMaterial, _previewHexes.Values.ToArray(),
+                _previewHexes.Count);
+        }
+
+        public void AddPreviewHex(HexCoordinate coordinate)
+        {
+            _previewHexes[coordinate] = GetMatrix(coordinate);
+        }
+
+        public void AddPreviewHexes(List<HexCoordinate> coordinates)
+        {
+            coordinates.ForEach(AddPreviewHex);
+        }
+
+        public void RemovePreviewHex(HexCoordinate coordinate)
+        {
+            _previewHexes.Remove(coordinate);
+        }
+
+        public void RemovePreviewHexes(List<HexCoordinate> coordinates)
+        {
+            coordinates.ForEach(RemovePreviewHex);
+        }
+
+        public void ClearPreviewHexes()
+        {
+            _previewHexes.Clear();
+        }
+
+        private static Matrix4x4 GetMatrix(HexCoordinate coordinate)
+        {
+            var position = HexGridManager.Instance.HexGrid.HexMap[coordinate].WorldPosition;
+            var quaternion = Quaternion.identity;
+            var scale = Vector3.one;
+
+            return Matrix4x4.TRS(position, quaternion, scale);
+        }
+    }
+}
---
+++ Assets/Scripts/Preview/PreviewManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using Hex;
+using Terrain;
+using UnityEngine;
+
+namespace Preview
+{
+    public class PreviewManager : MonoBehaviour
+    {
+        public static PreviewManager Instance;
+
+        private MeshFilter _meshFilter;
+        private MeshRenderer _meshRenderer;
+        private Mesh _previewMesh;
+        private Mesh _hexTemplate;
+
+        [SerializeField] private Material previewMaterial;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+
+            _meshFilter = gameObject.AddComponent<MeshFilter>();
+            _meshRenderer = gameObject.AddComponent<MeshRenderer>();
+
+            _hexTemplate = Util.GenerateHexagonMesh(MapGenerator.HexRadius, Color.white, MapGenerator.IsFlatTopped);
+
+            _previewMesh = new Mesh();
+            _meshFilter.mesh = _previewMesh;
+
+            _meshRenderer.material = previewMaterial;
+
+            // Position the preview slightly above the terrain to avoid z-fighting
+            transform.position = new Vector3(0, 0.05f, 0);
+        }
+
+        public void SetPreview(List<HexCoordinate> previewTiles)
+        {
+            _previewMesh = GeneratePreviewMesh(previewTiles, _hexTemplate);
+            _meshFilter.mesh = _previewMesh;
+        }
+
+        public void ClearPreview()
+        {
+            _previewMesh.Clear();
+        }
+
+        private static Mesh GeneratePreviewMesh(List<HexCoordinate> previewTiles, Mesh hexTemplate)
+        {
+            var vertices = new List<Vector3>();
+            var triangles = new List<int>();
+
+            var indexOffset = 0;
+
+            foreach (var tile in previewTiles)
+            {
+                var position = HexGridManager.Instance.HexGrid.HexMap[tile].WorldPosition;
+                AddHexagonToMesh(hexTemplate, position, ref vertices, ref triangles, indexOffset);
+                indexOffset += hexTemplate.vertexCount;
+            }
+
+            var mesh = new Mesh();
+            mesh.SetVertices(vertices);
+            mesh.SetTriangles(triangles, 0);
+            mesh.RecalculateNormals();
+
+            return mesh;
+        }
+
+        private static void AddHexagonToMesh(Mesh hexTemplate, Vector3 position, ref List<Vector3> vertices,
+            ref List<int> triangles,
+            int indexOffset)
+        {
+            for (var i = 0; i < hexTemplate.vertexCount; i++)
+            {
+                vertices.Add(hexTemplate.vertices[i] + position);
+            }
+
+            for (var i = 0; i < hexTemplate.triangles.Length; i++)
+            {
+                triangles.Add(hexTemplate.triangles[i] + indexOffset);
+            }
+        }
+    }
+}
