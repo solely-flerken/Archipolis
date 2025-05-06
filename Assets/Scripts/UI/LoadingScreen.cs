8d0dd684b2bace5f9881b9406dec17ff9f74b92e
solely-flerken <solely.flerken@proton.me>
2025-05-06 03:06:30 +0200

    💄 loading UI

---
+++ Assets/Scripts/UI/LoadingScreen.cs
@@ -0,0 +1,24 @@
+﻿using State;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UI
+{
+    public class LoadingScreen : MonoBehaviour
+    {
+        private VisualElement _root;
+        private Label _status;
+        private ProgressBar _progress;
+        
+        private void Start()
+        {
+            _root = GetComponent<UIDocument>().rootVisualElement;
+            _status = _root.Q<Label>("status");
+            _progress = _root.Q<ProgressBar>("progress");
+
+            var loadingStatus = new ViewModelLoadingStatus();
+            _status.Bind(loadingStatus.CurrentStatus, nameof(Label.text));
+            _progress.Bind(loadingStatus.ProgressLerp, nameof(ProgressBar.value));
+        }
+    }
+}
