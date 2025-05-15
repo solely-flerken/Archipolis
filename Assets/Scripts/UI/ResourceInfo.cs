7012681d5e7021a6daf34827f4e2dd431747ecd7
solely-flerken <solely.flerken@proton.me>
2025-05-16 00:51:38 +0200

    💄 display resourceType icon

--- Assets/Scripts/UI/ResourceInfo.cs
+++ Assets/Scripts/UI/ResourceInfo.cs
@@ -1,4 +1,5 @@
 using GameResources;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace UI
@@ -10,19 +11,54 @@ namespace UI
         private void Start()
         {
             IsVisibleInitially = true;
-            
+
             Root = GetComponent<UIDocument>().rootVisualElement;
             _container = Root.Q<VisualElement>("container");
 
             Root.style.display = DisplayStyle.None;
-            
+
             var resources = ResourceManager.Resources;
-            foreach (var resourceAmount in resources.Values)
+            foreach (var (resourceType, resourceAmount) in resources)
             {
+                // Container for resource image and amount label
+                var resourceEntry = new VisualElement
+                {
+                    style =
+                    {
+                        flexDirection = FlexDirection.Row,
+                        alignItems = Align.Center,
+                        justifyContent = Justify.Center
+                    }
+                };
+
+                // Define label
                 var resourceLabel = new Label();
                 var viewModel = new ViewModelResourceAmount(resourceAmount);
-                resourceLabel.Bind(viewModel.FormattedAmount, nameof(Label.text));
-                _container.Add(resourceLabel);
+
+                if (resourceType.icon != null)
+                {
+                    resourceLabel.Bind(viewModel.Amount, nameof(Label.text));
+
+                    var imageElement = new Image
+                    {
+                        image = resourceType.icon.texture,
+                        scaleMode = ScaleMode.ScaleToFit,
+                        style =
+                        {
+                            width = 40,
+                            height = 40
+                        }
+                    };
+                    resourceEntry.Add(imageElement);
+                }
+                else
+                {
+                    // If no icon is present, we display "resourceName: amount"
+                    resourceLabel.Bind(viewModel.FormattedAmount, nameof(Label.text));
+                }
+
+                resourceEntry.Add(resourceLabel);
+                _container.Add(resourceEntry);
             }
         }
     }
